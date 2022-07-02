1822c0d9ce0af0ad8a8e32d3cefe03905348bbdd
collins mbathi <perezcoolins@gmail.com>
2022-07-02 08:32:12 +0300

    tryed a new one

--- src/Emailvertify/Emailvertify.jsx
+++ src/Emailvertify/Emailvertify.jsx
@@ -3,19 +3,18 @@ import './Emailvertify.scss';
 import {Link,useParams} from 'react-router-dom';
 import {BASE_URL} from '../Utils/constants';
 import success from './check.png';
-import { useEffect,useState} from 'react';
+import { useEffect} from 'react';
 import axios from 'axios';
 
 const Emailvertify = () => {
   const param = useParams();
-   const [msg, setMsg] = useState('');
+
 
     useEffect(() => {
           const activationEmail = async () => {
               try {
                   const res = await axios.get(BASE_URL +`/user/${param.id}/verify/${param.token}`);
                   console.log(res.data);
-                   setMsg(res.message);
               } catch (err) {
                console.log(err);
               }
@@ -28,7 +27,7 @@ const Emailvertify = () => {
     <div className="container-success">
    <img src={success} alt="success_img" className='success-img' />
 
-  <h3>{msg}</h3>
+  <h3>The email is successfully vertified</h3>
   <Link to='/login'>
   <button className='success-button'>Login</button>	
   </Link>
