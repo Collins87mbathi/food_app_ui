06dc2872aeac41883e759d7bd3e460130c5d813e
collins mbathi <perezcoolins@gmail.com>
2022-07-02 08:01:00 +0300

    changed to hashroute

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './Home/Home'
 import Signin from './Signin/Signin'
 import Signup from './Signup/Signup'
